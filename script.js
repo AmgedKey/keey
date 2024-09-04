b425a78476672e41e6ee66598007e366324cc464
AmgedKey <qmzpn9@gmail.com>
2024-09-04 12:00:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -36,15 +36,15 @@ const gamePromoConfigs = {
         eventsDelay: 20000,
         attemptsNumber: 20
     },
-    CafeDash: {
-        appToken: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
-        promoId: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
+    TileTrio: {
+        appToken: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
+        promoId: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
         eventsDelay: 23000,
         attemptsNumber: 16
     },
-    GangsWars: {
-        appToken: 'b6de60a0-e030-48bb-a551-548372493523',
-        promoId: 'c7821fa7-6632-482c-9635-2bd5798585f9',
+    FluffCrusade: {
+        appToken: '112887b0-a8af-4eb2-ac63-d82df78283d9',
+        promoId: '112887b0-a8af-4eb2-ac63-d82df78283d9',
         eventsDelay: 40000,
         attemptsNumber: 23
     }, 
